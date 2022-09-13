8d59b1e90818949e468163f33f4e44a7292e053c
menyuu <101458830+menyuu@users.noreply.github.com>
2022-09-13 15:11:06 +0900

    Javaの練習

--- Capter02.java
+++ Capter02.java
@@ -2,6 +2,8 @@ package javaWcp;
 
 public class Capter02 {
 	public static void main(String[] args) {
-		System.out.println("こんにちは！");
+//		System.out.println("こんにちは！");
+		System.out.println("Hello WEBCAMP!");
+		System.out.println("Hello Java!");
 	}
 }
---
+++ Chapter05.java
@@ -0,0 +1,27 @@
+package javaWcp;
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 999999999999L;
+		System.out.println(long1);
+		double double1 = 0.123456789;
+		System.out.print(double1);
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+
+		Float float1 = 0.987654321F;
+		System.out.println(float1);
+		Integer integer1 = 157953;
+		System.out.println(integer1);
+		Character char1 = 'あ';
+		System.out.println(char1);
+
+		int[] array1 = new int[5];
+		array1[0] = 1;
+		array1[1] = 2;
+		array1[2] = 3;
+		array1[3] = 4;
+		array1[4] = 5;
+		System.out.println(array1[3]);
+	}
+}
